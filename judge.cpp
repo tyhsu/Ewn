ef527887c29e2b659bf824e2547fca3e3ea0d7b1
Jim <vic20087cjimlin@gmail.com>
2016-10-08 12:15:01 +0800

    add rollDice

--- judge.cpp
+++ judge.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
+using namespace std;
 
-void updatePlayer(char c){
+int currentPlayer[6] = {0,0,1,0,1,0};
+
+int rollDice() {
+
+	srand(time(NULL));
+	int a[2] = {0,0};
+	int result = rand()%6+1;	
+
+	if (currentPlayer[result-1] == 1) {
+
+		// this piece is alive!
+		for(int i = 0; i < 2; i++) a[i] = result;
+		return 1;
+	}
+	else {
+
+		// search for maybe other two.
+		for (int i = 1; i < result; i++) {
+			currentPlayer[i-1] == 1? a[0] = i: 0;
+		}
+
+		for (int i = result+1; i <= 6 && a[1]==0; i++) {
+			currentPlayer[i-1] == 1? a[1] = i : 0;
+		}
+
+		for (int i = 0; i < 2; i++) {
+			if (a[i] == 0) a[i] = a[1-i];
+		}
+		// return 1;
+	}
+	// printf("num: %d, ", result);
+	// for (int i = 0; i < 2; i++) {
+	// 	printf("%d ", a[i]);
+	// }
+	// printf("\n");
+	return 0;
+}
+
+int update(char c){
 
 	// to sort out player`s chess.
 	if (c != '0') {
@@ -11,19 +50,18 @@ void updatePlayer(char c){
 			// this Play is A
 			currentPlayer[c-'1'].exist = 0; 
 
-		} else {
+		}
+		else {
 
 			// this Play is B
 			currentPlayer[c-'A'].exist = 0;
 		}
 
-	} else {
+	}
+	else {
 
 		// nothing to deal with.
 	}
-}
-
-bool isGameEnd(){
 
 	// is this player been killed the game.
 	for (int i = 0; i < 6; i++) {
