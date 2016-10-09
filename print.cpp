9febe036cb9c73923ca6d187cbe8a0d94ead6038
Tia Hsu <dream040102@gmail.com>
2016-10-09 09:47:46 +0800

    change trivial things

--- print.cpp
+++ print.cpp
@@ -31,10 +31,10 @@ Game::Game()
 	int six = 0x111111;
 	srand( time(NULL) );
 	//Every position selects a number
-	while (six!=0) {
+	while (currPos<6) {
 		int number = rand()%6;	//0~6
 		if ((six & (1<<number)) >> number) {	//The number hasn't been used
-			board[ position[currPos][0] ][ position[currPos][1] ] = '1' + number;
+			this->board[ position[currPos][0] ][ position[currPos][1] ] = '1' + number;
 			currPos++;
 			six = six ^ (1<<number);	//The used number is discarded
 		}
@@ -44,37 +44,25 @@ Game::Game()
 	position = { {4,4}, {4,3}, {4,2}, {3,4}, {3,3}, {2,4} };
 	six = 0x111111;
 	srand( time(NULL) );
-	while (six!=0) {
+	while (currPos<6) {
 		int number = rand()%6;	//0~6
 		if ((six & (1<<number)) >> number) {	//The number hasn't been used
-			board[ position[currPos][0] ][ position[currPos][1] ] = 'A' + number;
+			this->board[ position[currPos][0] ][ position[currPos][1] ] = 'A' + number;
 			currPos++;
 			six = six ^ (1<<number);	//The used number is discarded
 		}
 	}
+	printBoard();
 }
 
 void Game::printBoard()
 {
 	cout << "------------------------------------" << endl;
 	cout << "The current board:" << endl;
-	if (!this->turn) {
-		for (int i=0; i<5; i++) {
-			cout << this->board[i][0];
-			for (int j=0; j<5; j++) {
-				cout << " " << this->board[i][j];
-			}
-			cout << endl;
-		}
-	}
-	else {
-		for (int i=4; i>=0; i--) {
-			cout << this->board[i][4];
-			for (int j=3; j>=0; j--) {
-				cout << " " << this->board[i][j];
-			}
-			cout << endl;
-		}
+	for (int i=0; i<5; i++) {
+		for (int j=0; j<5; j++)
+			cout << this->board[i][j];
+		cout << endl;
 	}
 	cout << "====================================" << endl;
 }
