6864e55006fd72f0832b3e4894deccf4c052a8e8
Jim <vic20087cjimlin@gmail.com>
2016-10-23 12:23:42 +0800

    add ai function

--- ewnAi.cpp
+++ ewnAi.cpp
@@ -2,3 +2,120 @@
 #include <cstdio>
 #include "game.h"
 #include "ewnAi.h"
+
+// Minimax
+
+
+// function minimax(board, height, maximizingPlayer)
+// 	if height==0	//node is a terminal node
+// 	{
+// 		loop though(player)
+// 		return the feature value of node	//heuristic value
+// 	}
+
+// 	curBoard := board	//copy
+// 	if maximizingPlayer
+// 		bestValue := -∞
+// 		// according to maximizingPlayer, know which player to play
+// 		for each possible move chs, dirct	//each child of node
+// 			nxtBoard := curBoard	//copy
+// 			nxtBoard.move(chs, dirct)
+// 			val := minimax(nxtBoard, height-1, FALSE)
+// 			bestValue := max(bestValue, val);
+// 		return bestValue
+// 	else
+// 		bestValue := +∞
+// 		for each possible move chs, dirct
+// 			nxtBoard := curBoard	//copy
+// 			nxtBoard.move(chs, dirct)
+// 			val := minimax(nxtBoard, height-1, TRUE)
+// 			bestValue := min(bestValue, val);
+// 		return bestValue
+
+// (* Initial call for maximizing player *)
+// minimax(origin, height, TRUE)
+
+int EwnAI::minimax(Game& current_game, int height) {
+	
+	// check if end;
+	if (height == 0) {
+		return evaluate(current_game);
+	}
+	else {
+		height--;
+	}
+
+	int bestValue = 0;
+	// if false(0): A`s turn, find max, else find min.
+	if (current_game->turn_ == false) {
+
+		bestValue = -1e9;
+		// for #chess and direction 
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				Game next_step = current_game;
+				next_step.move(i,j);
+				
+				// switch player;
+				next_step->switchPlayer();
+
+				// return child value.
+				int child_value = minimax(next_step, height-1);
+				bestValue = min(bestValue, child_value);
+			}
+		}
+		return bestValue;
+	}
+	else {
+
+		bestValue = 1e9;
+		// for #chess and direction 
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 3; j++) {
+				Game next_step = current_game;
+				next_step.move(i,j);
+				
+				// switch player;
+				next_step->switchPlayer();
+
+				// return child value.
+				int child_value = minimax(next_step, height-1);
+				bestValue = min(bestValue, child_value);
+			}
+		}
+		return bestValue;
+	}
+}
+
+int EwnAI::evaluate(Game& current_game){
+	int val = 0;
+
+	for(int i = 0; i < current_game.chsNumA_; i++){
+		pos temp;
+		temp.first = current_game.playerA_[i].x;
+		temp.second = current_game.playerA_[i].y;
+		val += hv[temp];
+	}
+
+	return val;
+}
+
+EwnAI::EwnAI(){
+
+	// pos: a <int,int> , <x,y> coord;
+	// insert to map => hv;
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0;j < 5; j++) {
+
+			// get the smallest of (x,y)
+			int k = (i > j ? j : i);
+
+			k = k * k;
+			pos chess_value;
+			chess_value.first = i;
+			chess_value.second = j;
+
+			hv.insert(make_pair(chess_value, k));
+		}
+	}
+}
--- ewnAi.h
+++ ewnAi.h
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <cstdio>
+#include <utility>
+#include <map>
 #include "game.h"
 using namespace std;
+
+typedef pair<int, int> pos;
+typedef map<pos , int> heuristic_value;
 
 class EwnAI
 {
@@ -9,9 +14,13 @@ public:
 	//the result after the simulation
 	Chess chosenChs_;
 	int chosenDirct_;
+	heuristic_value hv;
 
 	EwnAI();
 	~EwnAI();
 	int feature(char board[5][5]);
 	int simulate(int height);
+	
+	int minimax(Game& origin_game, int h);
+	int evaluate(Game& current_game);
 };
