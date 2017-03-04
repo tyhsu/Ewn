0fcb23bb9e26fcade7dfd3391b9c8b23392d9652
Chun-Liang Yang <clyang0318@linux1.cs.nctu.edu.tw>
2017-03-04 12:33:05 +0800

    modify structure

--- mcts.h
+++ mcts.h
@@ -4,19 +4,24 @@ const float eps = 1e-6;
 using namespace std;
 class UCT {
 public:
-    Tree_node * root;
-    int iteration;  // nodes
-    int max_iterations;
-    int simulation_depth;
     float uct_k;    // uct param
     UCT ();
     Tree_node * get_best_child ( Tree_node * node, float uct_k );
-
     Tree_node * get_most_visited_child ( Tree_node * node );
-
-    // mcts main 
-    Movement run(const Game& current_state, unsigned int seed = 1, vector<Game>* explored_states = nullptr);
 }
 
-
+class MCTS {
+public:
+	bool ai_side;
+	char ai_symbol;
+	int iteration;  // nodes
+    int max_iterations;
+    int simulation_depth;
+	Tree_node * root;
+	UCT uct;
+	MCTS(Game& game, int dice);
+	// mcts main 
+	Movement run(const Game& current_state, unsigned int seed = 1, vector<Game>* explored_states = nullptr);
+	
+}
 // src: https://github.com/memo/ofxMSAmcts/blob/master/src/ofxMSAmcts.h
