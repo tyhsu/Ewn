739d1336ecb08ec5ef6ce3b4ea7c052caeb5415c
Jim <vic20087cjimlin@gmail.com>
2017-03-30 00:32:22 +0800

    add: Init simulation class

---
+++ simulation.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include "game.h"
+#include "simulation.h"
+
+Simulation::Simulation()
+{
+    return 0;
+}
+
+Simulation::Simulation(Game simu_game, int type)
+{
+    return 0;
+}
---
+++ simulation.h
@@ -0,0 +1,14 @@
+#ifndef SIMULATION_H
+#define SIMULATION_H
+
+#include "game.h"
+
+class Simulation
+{
+public:
+    Simulation() {}
+    Simulation(Game simu_game)
+    ~Simulation() {}
+};
+
+#endif
